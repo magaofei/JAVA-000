a61b4de159dfc69dc9ddd76c5b06c69d22ee6bf6
magaofei <mamian521@gmail.com>
2021-01-03 17:06:38 +0800

    使用 AOP 替换动态代理

--- Week_09/rpc01/rpcfx-core/src/main/java/io/kimmking/rpcfx/client/Rpcfx.java
+++ Week_09/rpc01/rpcfx-core/src/main/java/io/kimmking/rpcfx/client/Rpcfx.java
@@ -13,6 +13,9 @@ import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
 import org.apache.curator.retry.ExponentialBackoffRetry;
 import org.apache.zookeeper.server.ZooKeeperServer;
+import org.springframework.cglib.proxy.Enhancer;
+import org.springframework.cglib.proxy.MethodInterceptor;
+import org.springframework.cglib.proxy.MethodProxy;
 
 import java.io.IOException;
 import java.lang.reflect.InvocationHandler;
@@ -61,7 +64,79 @@ public final class Rpcfx {
     public static <T> T create(final Class<T> serviceClass, final String url, Filter... filters) {
 
         // 0. 替换动态代理 -> AOP
-        return (T) Proxy.newProxyInstance(Rpcfx.class.getClassLoader(), new Class[]{serviceClass}, new RpcfxInvocationHandler(serviceClass, url, filters));
+        Enhancer enhancer = new Enhancer();
+        //enhancer.setSuperclass(RpcAopfx.class);
+        enhancer.setInterfaces(new Class[] {serviceClass});
+        enhancer.setCallback(new RpcfxCglibInvocationHandler(serviceClass,url));
+        return (T) enhancer.create();
+//        return (T) Proxy.newProxyInstance(Rpcfx.class.getClassLoader(), new Class[]{serviceClass}, new RpcfxInvocationHandler(serviceClass, url, filters));
+
+    }
+
+    public static class RpcfxCglibInvocationHandler implements MethodInterceptor {
+
+
+        public static final MediaType JSONTYPE = MediaType.get("application/json; charset=utf-8");
+
+        private final Class<?> serviceClass;
+        private final String url;
+        private final Filter[] filters;
+
+        public <T> RpcfxCglibInvocationHandler(Class<T> serviceClass, String url, Filter... filters) {
+            this.serviceClass = serviceClass;
+            this.url = url;
+            this.filters = filters;
+        }
+
+        // 可以尝试，自己去写对象序列化，二进制还是文本的，，，rpcfx是xml自定义序列化、反序列化，json: code.google.com/p/rpcfx
+        // int byte char float double long bool
+        // [], data class
+
+        @Override
+        public Object intercept(Object o, Method method, Object[] objects, MethodProxy methodProxy) throws Throwable {
+
+            // 加filter地方之二
+            // mock == true, new Student("hubao");
+
+            RpcfxRequest request = new RpcfxRequest();
+            request.setServiceClass(this.serviceClass.getName());
+            request.setMethod(method.getName());
+            request.setParams(objects);
+
+            if (null!=filters) {
+                for (Filter filter : filters) {
+                    if (!filter.filter(request)) {
+                        return null;
+                    }
+                }
+            }
+
+            RpcfxResponse response = post(request, url);
+
+            // 加filter地方之三
+            // Student.setTeacher("cuijing");
+
+            // 这里判断response.status，处理异常
+            // 考虑封装一个全局的RpcfxException
+
+            return JSON.parse(response.getResult().toString());
+        }
+
+        private RpcfxResponse post(RpcfxRequest req, String url) throws IOException {
+            String reqJson = JSON.toJSONString(req);
+            System.out.println("req json: "+reqJson);
+
+            // 1.可以复用client
+            // 2.尝试使用httpclient或者netty client
+            OkHttpClient client = new OkHttpClient();
+            final Request request = new Request.Builder()
+                    .url(url)
+                    .post(RequestBody.create(JSONTYPE, reqJson))
+                    .build();
+            String respJson = client.newCall(request).execute().body().string();
+            System.out.println("resp json: "+respJson);
+            return JSON.parseObject(respJson, RpcfxResponse.class);
+        }
 
     }
 
